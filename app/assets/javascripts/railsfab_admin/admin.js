1fa223fb79e253ae02757d57f19f1a3901b15436
Bhaskar <bhaskar8088@gmail.com>
2013-12-01 16:52:03 +0530

    Jquery-rails

--- app/assets/javascripts/railsfab_admin/admin.js
+++ app/assets/javascripts/railsfab_admin/admin.js
@@ -1,10 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$("input[type=submit]").click(function(){
-    if($(".cb-enabled-field").length){
+
+$(".submit_btn").click(function(e){
+    if(!$(".cb-enabled-field").length){
         e.preventDefault();
-        alert("None of the fields are enabled to be updated. Please check the boxes to enable those checked fields to be updated")
+        alert("Please enable some of the fields(by checking) so only selected fields will be updated");
     }
 });
-    
