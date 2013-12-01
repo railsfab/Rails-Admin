8edf40f7b0194e3b627443aa247b303309a55fc2
Bhaskar <bhaskar8088@gmail.com>
2013-12-01 17:33:25 +0530

    Fixed js: Submit edit page only when any of the fields selected

--- app/assets/javascripts/railsfab_admin/admin.js
+++ app/assets/javascripts/railsfab_admin/admin.js
@@ -3,7 +3,7 @@
 
 
 $(".submit_btn").click(function(e){
-    if(!$(".cb-enabled-field").length){
+    if(!$(".cb-enable-field:checked").length){
         e.preventDefault();
         alert("Please enable some of the fields(by checking) so only selected fields will be updated");
     }
