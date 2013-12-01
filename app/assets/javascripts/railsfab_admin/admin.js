1b72040fa3a589e9260419ad8951f1b72ac42e5e
Bhaskar <bhaskar8088@gmail.com>
2013-12-01 14:10:26 +0530

    Dont submit if none of the fields are enabled/selected for update

--- app/assets/javascripts/railsfab_admin/admin.js
+++ app/assets/javascripts/railsfab_admin/admin.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$("input[type=submit]").click(function(){
+    if($(".cb-enabled-field").length){
+        e.preventDefault();
+        alert("None of the fields are enabled to be updated. Please check the boxes to enable those checked fields to be updated")
+    }
+});
+    
